186f1af1da17f2ebb8a9d66e094904ab72c67ead
Alex F <adfarries@gmail.com>
2019-07-09 19:36:42 +1000

    WIP rework importer to process each chat

--- src/zandt/importer.clj
+++ src/zandt/importer.clj
@@ -1,30 +1,39 @@
-;; This namespace is for importing raw export data into a sqlite
-;; database
 (ns zandt.importer
-  (:require [cheshire.core :refer [parse-string]]
+  (:require [clojure.string :refer [trim-newline]]
+            [cheshire.core :refer [parse-string]]
             [clojure.java.jdbc :refer :all]))
 
-;; http://clojure-doc.org/articles/ecosystem/java_jdbc/using_sql.html
-;; https://github.com/clojure/java.jdbc
-;; https://grishaev.me/en/clj-sqlite/
 (def sqlite-db-spec
-  {:dbtype   "sqlite"
-   :dbname    "zandt"})
+  {:classname   "org.sqlite.JDBC"
+   :subprotocol "sqlite"
+   :subname     "db/zandt.db"})
 
 (defn json-string->data-map [export-file-path]
   (parse-string (slurp export-file-path) true))
 
-(defn update-or-insert-user-data [data-map]
-  (let [user-data (-> data-map
-                      (get "personal_information")
-                      (select-keys :user_id :first_name :last_name))]
-    (jdbc/insert! sqlite-db-spec :users {:telegram_id (:user_id user-data)
-                                         :first_name (:first_name user-data)
-                                         :last_name (:last_name user-data)})))
+(defn create-zandt-db
+  (try (db-do-commands sqlite-db-spec
+                       (execute! (slurp "db/prepare_zandt_sqlite_db.sql")))
+       (catch Exception e
+         (println (.getMessage e)))))
 
-;; Add function that:
-;;   - Inserts messages
-;;   - Scans through and either inserts word or updates word count
-;;   - Scans through and inserts emojis found in messages
+;; TODO: Add function that:
+;;   - Processes each chat in a list
+;;     - Inserts the user from the chat
+;;     - Scans through, collecting each unique word and how frequently it occurs
+;;       - Will check db if word exists,
+;;         IF it already exists
+;;           icrement the count of that word by its frequency in the message
+;;         ELSE
+;;           Insert the word with its frequency
+;;     - Scans through and inserts emojis found in messages
+;;       - Finds emojis with emoji matching regex e.g. "(<U\\+\\w+?>)"
+
+
+(defn -main [arg]
+  (let [export-data (json-string->data-map arg)]
+    (create-zandt-db)))
 
 (def export-data (json-string->data-map "./test/fixtures/minimal_export_data.json"))
+
+(slurp "db/prepare_zandt_sqlite_db.sql")
