a74c4455646b953a155ea6ab71b45858564a1140
Alex F <adfarries@gmail.com>
2019-07-12 15:28:38 +1000

    WIP add unit tests for data import transformations
    I'm undecided how to approach this:
    - I can remove the need to mock a connection and responses from the
    database by isolating the side-effecting code to the jdbc actions
    themselves...but these functions already feel like they should be
    private functions and have no real purpose outside this namespace.
    
    - Is there value in having a live sql connnection in my test
    environment for end to end testing regardless?

--- test/zandt/importer_test.clj
+++ test/zandt/importer_test.clj
@@ -1,7 +1,35 @@
 (ns zandt.importer-test
   (:require [clojure.test :refer :all]
-            [zandt.importer :refer :all]))
+            [zandt.importer :refer [message->message-data
+                                    message->word-and-frequency]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; TODO: Should I be trying to test more 'pure' functions, and only doing side-effecting
+;; actions with the return values of these tested functions? Or should I build smaller private
+;; namespace functions that I trust, and then either mock the db or build a test-connection to
+;; test the main interface?
+(deftest test-message->message-data
+    (let [test-message {:id 99999
+                        :type "message"
+                        :date "2018-03-10T17:45:50"
+                        :edited "1970-01-01T10:00:00"
+                        :from "Some one"
+                        :from_id 12345
+                        :text "Random message"}]
+      (is (= (message->message-data test-message)
+             ({:telegram_id 12345 :text "Random message"})))))
+
+(deftest test-message->word-and-frequency
+  (testing "It counts words, is case-insensitive and ignores emojis"
+    (let [test-message {:id 99999
+                        :type "message"
+                        :date "2018-03-10T17:45:50"
+                        :edited "1970-01-01T10:00:00"
+                        :from "Some one"
+                        :from_id 12345
+                        :text "And how and what. Why? ☺️ "}]
+      (is (= (message->word-and-frequency test-message)
+             (#{{:word "and" :count 2}
+                {:word "how" :count 1}
+                {:word "what" :count 1}
+                {:word "why" :count 1}
+                }))))))
