a15a26b5a4739def489fc975ef76c98cd7967948
Alex F <adfarries@gmail.com>
2019-08-01 18:50:57 +1000

    Add update-or-insert
    http://clojure-doc.org/articles/ecosystem/java_jdbc/using_sql.html#updating-or-inserting-rows-conditionally

--- src/zandt/importer.clj
+++ src/zandt/importer.clj
@@ -1,5 +1,5 @@
 (ns zandt.importer
-  (:require [clojure.string :refer [trim-newline lower-case]]
+  (:require [clojure.string :refer [trim-newline lower-case escape]]
             [cheshire.core :refer [parse-string]]
             [clojure.java.jdbc :refer :all]))
 
@@ -53,3 +53,34 @@
                               :count count})
          emoji-frequencies)))
 
+(defn update-or-insert!
+  "Updates columns or inserts a new row in the specified table"
+  [db table row where-clause]
+  (with-db-transaction [t-con db]
+    (let [result (update! t-con table row where-clause)]
+      (if (zero? (first result))
+        (insert! t-con table row)
+        result))))
+
+(update-or-insert! mysql-db :fruit
+                   {:name "Cactus" :appearance "Spiky" :cost 2000}
+                   ["name = ?" "Cactus"])
+;; inserts Cactus (assuming none exists)
+(update-or-insert! mysql-db :fruit
+                   {:name "Cactus" :appearance "Spiky" :cost 2500}
+                   ["name = ?" "Cactus"])
+;; updates the Cactus we just inserted
+
+(defn -main [arg]
+  (let [export-data (json-string->data-map arg)
+        chats (get-in export-data '(:chats :list))]
+    (create-zandt-db)))
+
+(def export-data (json-string->data-map "./test/fixtures/minimal_export_data.json"))
+
+(slurp "db/prepare_zandt_sqlite_db.sql")
+
+;; TODO: Upsert/take current value of word
+;; UPDATE OR IGNORE ... (increment current value by frequency from message)
+;; INSERT OR IGNORE ... (add to default 0 the value by frequency from message)
+;; When finding a word before incrementing...check by user too...?
