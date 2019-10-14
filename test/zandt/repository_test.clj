76f864075718b60bfa074b3e544352ba92706b4a
Alex F <adfarries@gmail.com>
2019-10-14 19:30:48 +1100

    Fix test-find-or-update-user!

--- test/zandt/repository_test.clj
+++ test/zandt/repository_test.clj
@@ -2,48 +2,37 @@
   (:require [zandt.repository :refer [create-zandt-db
                                       update-or-insert!
                                       find-or-update-user!]]
-            [zandt.sqlite :refer [sqlite-db-spec
+            [zandt.sqlite :refer [db
+                                  sqlite-db-spec
                                   test-sqlite-db-spec
                                   establish-sqlite-connection
                                   close-sqlite-connection]]
             [clojure.java.io :refer [delete-file]]
-            [clojure.java.jdbc :refer [query execute!]]
+            [clojure.java.jdbc :refer [query]]
             [clojure.test :refer :all]))
 
 (defn with-test-db [tests]
   (with-redefs
     [sqlite-db-spec test-sqlite-db-spec]
     sqlite-db-spec)
+  (prn "### Setting up test sqlite db ###")
   (establish-sqlite-connection)
   (create-zandt-db)
   (tests)
+  (prn "### tearing down sqlite db ###")
   (close-sqlite-connection))
 
 (use-fixtures :once with-test-db)
 
 (deftest test-find-or-update-user!
-  (let [user-data {:username "Falla",
+  (let [user-data {:username "Falla"
                    :telegram_id 8888888888}]
     (find-or-update-user! user-data)
-    (is (= (query test-sqlite-db-spec
-                  "SELECT * FROM users WHERE telegram_id = ?"
-                  (get user-data :telegram_id))
-           "hi"))))
+    (is (= (query db
+                  ["SELECT username, telegram_id FROM users WHERE telegram_id = ?"
+                   (get user-data :telegram_id)])
+           '({:username "Falla" :telegram_id 8888888888})))))
 
 (deftest test-find-or-update-message!)
 (deftest test-create-or-increment-word!)
 (deftest test-create-or-increment-emoji!)
-
-(deftest test-message->message-data
-    (let [message {:id 99999
-                        :type "message"
-                        :date "2018-03-10T17:45:50"
-                        :edited "1970-01-01T10:00:00"
-                        :from "Some one"
-                        :from_id 12345
-                   :text "Random message"}
-          user-id 999]
-      (is (= (message->message-data message user-id)
-             {:telegram_id 99999
-              :user_id 999
-              :text "Random message"}))))
