30480aaa65dd357ecbad7df16f71f41c17878b57
Alex F <adfarries@gmail.com>
2019-11-18 20:53:43 +1100

    Remove message_id from tables
    No longer makes sense with current data model

--- db/tables/emojis.sql
+++ db/tables/emojis.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS emojis (
     id INTEGER PRIMARY KEY,
-    message_id INTEGER,
     user_id INTEGER,
     frequency INTEGER DEFAULT 0,
     emoji TEXT NOT NULL,
-    FOREIGN KEY (message_id) REFERENCES messages (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
--- db/tables/words.sql
+++ db/tables/words.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS words (
     id INTEGER PRIMARY KEY,
-    message_id INTEGER,
     user_id INTEGER,
     frequency INTEGER DEFAULT 0,
     word TEXT NOT NULL,
-    FOREIGN KEY (message_id) REFERENCES messages (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
